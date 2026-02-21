3a3c5fba8a05c92297284a12d5dfa2a937e35bd9
Mi-dels <lamyogunjobi85@gmail.com>
2026-02-21 15:41:32 -0800

    second commit

--- Group12.py
+++ Group12.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 class Translator:
     def __init__(self,):
@@ -103,14 +104,96 @@ class Phrasebook:
             for original,translation in self.phrases.items():
                 print(f"{original} = {translation}")
 
-    def deletePhrase(self)
+    def deletePhrase(self,original):
+        if original in self.phrases:
+            del self.phrases[original]
+            self.savePhrases()
+        else:
+            print("Phrase not found.")
         
 class Dictionary:
-    def __init__(self):
-        pass
+    def __init__(self,):
+        self.cache = {}
+        self.history = []
+
+    def getDictionry(self,word):
+        word = word.lower()
+
+        if word in self.cache:
+            return self.cache[word]
+        
+        url = f"https://api.dictionaryapi/api/v2/entries/en/{word}"
+
+        try:
+            response = requests.get(url)
+
+            if response.status_code != 200:
+                return {
+                    "word": word,
+                    "definitions":"Not available",
+                    "syllables":"Not available",
+                    "pronounciation":"Not available",
+                    "part_of_speech":"Not available"
+                }
+
+            
+            data = response.json()
+            entry = data[0]
+            word = entry.get("word",word)
+            pronounciation = entry.get("phonetic","Not available")
+            definition = entry["meanings"][0]["definitions"][0]["definition"]
+            part_of_speech = entry["meanings"][0]["partOfSpeech"]
+            syllables = entry.get("syllables", {}).get("list","Not available")
+
+            details = {
+                "word" : word,
+                "definition" : definition,
+                "pronounciation" : pronounciation,
+                "syllables":syllables,
+                "part_of_speech": part_of_speech
+
+            }
+            self.cache[word] = details
+            return details
+        except:
+            return{
+                "word": word,
+                "defiinition": "Error loading file.......",
+                "pronounciation":"Not available",
+                "syllables":"Not available",
+                "part_of_speech":"Not availaable"
+
+            }
+
+
+        
+
+       
 class Quiz:
-    def __init__(self):
-        pass
+    def __init__(self,phrasebook,stats_file="quiz.stats.json"):
+        self.phrasebook = phrasebook
+        self.stats_file = stats_file
+        self.stats = self.loadStats()  
+
+    def loadStats(self):
+        try:
+            with open(self.stats_file, "r") as file :
+                return json.load(file)
+        except FileNotFoundError:
+            return {"Correct" : 0, "Wrong" : 0}
+        
+    def saveStats(self):
+        with open(self.stats_file, "w") as file:
+            return json.dump(self.stats,file, indent=4)
+        
+    def takeQuiz(self):
+        if not self.phrasebook.phrases:
+            print("No phrases availble for quiz. Add some first.")
+            return
+        
+        original, translation = random.choice(list(self.phrasebook.phrases.items()))
+
+        print()
 class Stats:
     def __init__(self):
         pass
